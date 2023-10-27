d476c97bde3470a1521815d0005aae4efc54f772
Lucas Sta Maria <lucas@priime.dev>
2023-10-27 02:35:42 -0400

    feat: add `Config` struct and builder
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ src/config.rs
@@ -0,0 +1,79 @@
+/// Contains configuration information for the TensorDock client. The
+/// authorization keys and tokens can be retrieved from
+/// `https://marketplace.tensordock.com/api`.
+#[derive(Debug, PartialEq, Eq)]
+pub struct Config {
+    /// The authorization key uuid.
+    pub key: String,
+    /// The authorization token uuid.
+    pub token: String,
+    /// An optional description describing the purpose of this config /
+    /// authorization set.
+    pub description: Option<String>,
+}
+
+impl Config {
+    pub fn builder() -> ConfigBuilder {
+        ConfigBuilder::default()
+    }
+}
+
+/// Utility struct used in intermediary construction of a `Config` via the
+/// builder creational pattern.
+#[derive(Default)]
+pub struct ConfigBuilder {
+    key: Option<String>,
+    token: Option<String>,
+    description: Option<String>,
+}
+
+impl ConfigBuilder {
+    /// Produces a new `ConfigBuilder` with the `key` field set.
+    pub fn key(self, key: String) -> Self {
+        Self {
+            key: Some(key),
+            ..self
+        }
+    }
+
+    /// Produces a new `ConfigBuilder` with the `token` field set.
+    pub fn token(self, token: String) -> Self {
+        Self {
+            token: Some(token),
+            ..self
+        }
+    }
+
+    /// Produces a new `ConfigBuilder` with the `description` field set.
+    pub fn description(self, description: String) -> Self {
+        Self {
+            description: Some(description),
+            ..self
+        }
+    }
+
+    /// Build the `ConfigBuilder`, producing a `Config`, and assigning empty string
+    /// values to the `key` and `token` values if not set.
+    pub fn build(self) -> Config {
+        Config {
+            key: self.key.unwrap_or_else(|| String::from("")),
+            token: self.token.unwrap_or_else(|| String::from("")),
+            description: self.description,
+        }
+    }
+}
+
+#[test]
+fn config_builder_test() {
+    let expected = Config {
+        key: String::from("abcd"),
+        token: String::from("efgh"),
+        description: None,
+    };
+    let result = Config::builder()
+        .key(String::from("abcd"))
+        .token(String::from("efgh"))
+        .build();
+    
+    assert_eq!(expected, result);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
-#[must_use]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod config;
+pub mod error;
+
+/// Client for the TensorDock Marketplace API, found here:
+/// https://documenter.getpostman.com/view/20973002/2s8YzMYRDc
+pub struct TensorDock {
+    pub config: config::Config,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl TensorDock {
+    /// Create a new `TensorDock` client with the given authorization
+    /// key and authorization token.
+    pub fn new(config: config::Config) -> Self {
+        Self {
+            config,
+        }
     }
 }
