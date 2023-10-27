afdf16655a1628eb0cb7b235e4fdbd3491accd76
Lucas Sta Maria <lucas@priime.dev>
2023-10-27 02:35:50 -0400

    feat: add `TensorDockAPIError` struct
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ src/error.rs
@@ -0,0 +1,13 @@
+use std::{error::Error, fmt::{self, Display}};
+
+/// An `Error` type for reporting errors related to TensorDock API interactions.
+#[derive(Debug)]
+pub struct TensorDockAPIError {
+    pub message: String,
+}
+
+impl Display for TensorDockAPIError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Encountered TensorDock API Error: {}", self.message)
+    }
+}
